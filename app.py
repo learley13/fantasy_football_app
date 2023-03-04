bea1feda6c98cf4ff3cb70abad1eb1a03c1ea9d6
Liam Earley <liamearley@Liams-MacBook-Pro.local>
2023-03-04 13:59:17 -0500

    reverting

--- app.py
+++ app.py
@@ -5,43 +5,58 @@ import pandas as pd
 import matplotlib as mpl
 import gunicorn  # whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
 from whitenoise import WhiteNoise  # for serving static files on Heroku
-from dash import Dash, html, dcc
-import plotly.express as px
-from espn_api.football import League
 
 # Instantiate dash app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-league = League(
-    league_id=57605111,
-    year=2022,
-    espn_s2="AEBljLTvAWQpfgm071iZfMNA%2FzfweMFw2Ph1IZEOka3wjXos0vLrjA5gi1ls739ioa6FQCTwA9PE6o12qVzuof%2BYEnoJ0SDbSoXlvpAbYQjQIogEi0qlazZ%2Bm0zP8Xsn%2BE6Fh39ImDCUgVto0mitLZU6efBNze7QqMC2giomtIKGTe4bJJ9G4boqtaU5144PB5MnvEuLnRZx2py2GmXwKscFL6dNiOEFpsC1f4dozu8tw0cjnJdjtQOaPzKAGsWdUrR62CpWtAPcGngtB5My7Rga",
-    swid="{1124F5EB-909C-4747-A269-E0C8729AF9C9}",
-)
+# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
+server = app.server
 
-team_standings = {}
-for team in league.teams:
-    team_standings[team.team_name] = team.final_standing
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku)
+server.wsgi_app = WhiteNoise(server.wsgi_app, root="static/")
 
-standings = (
-    pd.DataFrame.from_dict(team_standings, orient="index", columns=["Final Standing"])
-    .reset_index()
-    .sort_values(by="Final Standing", ascending=False)
-)
 
-lame_bar_chart = px.bar(standings, x="index", y="Final Standing")
+# Define Dash layout
+def create_dash_layout(app):
+    # Set browser tab title
+    app.title = "Liam's App"
 
-app.layout = html.Div(
-    children=[
-        html.H1(children="First Pass"),
-        html.Div(
-            children="""
-        Sample subtitle.
-    """
-        ),
-        dcc.Graph(id="first-graph", figure=lame_bar_chart),
-    ]
-)
+    # Header
+    header = html.Div(
+        [html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()]
+    )
 
+    # Body
+    body = html.Div(
+        [
+            dcc.Markdown(
+                """ ## I'm ready to serve static files on Heroku. Just look at this! """
+            ),
+            html.Br(),
+            html.Img(src="charlie.png"),
+        ]
+    )
+
+    # Footer
+    footer = html.Div(
+        [
+            html.Br(),
+            html.Br(),
+            dcc.Markdown(
+                """ ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)"""
+            ),
+        ]
+    )
+
+    # Assemble dash layout
+    app.layout = html.Div([header, body, footer])
+
+    return app
+
+
+# Construct the dash layout
+create_dash_layout(app)
+
+# Run flask app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False, host="0.0.0.0", port=8050)
